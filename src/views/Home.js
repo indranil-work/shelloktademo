75432a8e39e0291f54f3ddbaa44a26b2a9df4f3f
Matt Plant <matthew.plant@okta.com>
2025-01-20 16:58:09 +0000

    removed redundant code from home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import waveImage from '../assets/background-bottom.png';
 import decorativeImage from '../assets/background-right.png';
@@ -9,129 +9,8 @@ const Home = () => {
   const { selectedJourney, setSelectedJourney, selectedLocale, getClientConfig } = useUser();
   const { loginWithRedirect, isAuthenticated, user } = useAuth0();
   const history = useHistory();
-  const { search, hash } = useLocation();
+  const { search } = useLocation();
   const parsedSearch = new URLSearchParams(search);
-  const CACHE_KEY_PREFIX = '@@auth0spajs@@';
-
-  const decodeB64 = (input) => {
-    return decodeURIComponent(
-      atob(input)
-        .split('')
-        .map(c => {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join('')
-    );
-  }
-  const urlDecodeB64 = (input) => {
-    return decodeB64(input.replace(/_/g, '/').replace(/-/g, '+'));
-  }
-    
-
-  const idTokendecoded = [
-    'iss',
-    'aud',
-    'exp',
-    'nbf',
-    'iat',
-    'jti',
-    'azp',
-    'nonce',
-    'auth_time',
-    'at_hash',
-    'c_hash',
-    'acr',
-    'amr',
-    'sub_jwk',
-    'cnf',
-    'sip_from_tag',
-    'sip_date',
-    'sip_callid',
-    'sip_cseq_num',
-    'sip_via_branch',
-    'orig',
-    'dest',
-    'mky',
-    'events',
-    'toe',
-    'txn',
-    'rph',
-    'sid',
-    'vot',
-    'vtm'
-  ];
-
-  const decodeToken = (token) => {
-    const parts = token.split('.');
-    const [header, payload, signature] = parts;
-  
-    if (parts.length !== 3 || !header || !payload || !signature) {
-      throw new Error('ID token could not be decoded');
-    }
-    const payloadJSON = JSON.parse(urlDecodeB64(payload));
-    const claims = { __raw: token };
-    const user = {};
-    Object.keys(payloadJSON).forEach(k => {
-      claims[k] = payloadJSON[k];
-      if (!idTokendecoded.includes(k)) {
-        user[k] = payloadJSON[k];
-      }
-    });
-    return {
-      encoded: { header, payload, signature },
-      header: JSON.parse(urlDecodeB64(header)),
-      claims,
-      user
-    };
-  };
-
-  const createCacheKey = (client_id, audience, scope) => {
-    return `${CACHE_KEY_PREFIX}::${client_id}::${audience}::${scope}`;
-  }
-
-  const createCacheEntry = (idToken, accessToken, expiresIn, audience, scope, clientId, refreshToken) => {
-    const decodedToken = decodeToken(idToken);
-
-    const now = Date.now();
-    const expiresInTime = Math.floor(now / 1000) + expiresIn;
-
-    const expirySeconds = Math.min(
-      expiresInTime,
-      decodedToken.claims.exp
-    );
-
-    return {
-      body: {
-        id_token: idToken,
-        access_token: accessToken,
-        expires_in: expiresIn,
-        decodedToken: decodedToken,
-        audience: audience,
-        scope: scope,
-        client_id: clientId,
-        refresh_token: refreshToken
-      },
-      expiresAt: expirySeconds
-    }
-  }
-
-  useEffect(() => {
-    async function loginWithMagicLink(parsedHash) {
-      let clientId = 'idM0BINRTdeMuUKsTw00U5vRmojFRGcP';
-      let audience = 'https://demo.okta.com';
-      let cacheKey = createCacheKey(clientId, audience, parsedHash.get('scope'));
-      let cacheEntry = createCacheEntry(parsedHash.get('id_token'), parsedHash.get('access_token'), parsedHash.get('expires_in'), audience, parsedHash.get('scope'), clientId);
-      localStorage.setItem(cacheKey, JSON.stringify(cacheEntry));
-      //Cookies.set(`auth0.${clientId}.is.authenticated`, true);
-      document.cookie = `auth0.${clientId}.is.authenticated=true`;
-      history.push('/');
-    }
-
-    if(hash){
-      let parsedHash = new URLSearchParams(hash.replace('#',''));
-      loginWithMagicLink(parsedHash);
-    }
-  }, [hash]);
 
   if(parsedSearch.has('error')){
     alert(`Oops... ${parsedSearch.get('error_description')} - redirecting back to login`);
